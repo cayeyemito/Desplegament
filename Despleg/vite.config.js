cfd828451f7cfc66891f213f7cd666553cac690d
cayeyemito <cayetanoym@gmail.com>
2026-01-20 09:51:26 +0100

    Prueba

---
+++ Despleg/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Desplegament/',
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+
+  server: {
+    port: 5173,
+    open: true
+  },
+
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
+})
